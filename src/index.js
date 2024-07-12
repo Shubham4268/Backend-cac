620b0a67a36864f83c308d58eba886ff43c84707
Shubham Joshi <shubhamdjoshi31@gmail.com>
2024-07-12 18:26:42 +0530

    12-07 commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error: ",error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log("Server is running on port: ", process.env.PORT);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !! ", err);
+})
 
 
 

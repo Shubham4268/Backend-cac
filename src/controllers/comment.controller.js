f2adf26fc00db6325e59c8744330d802a1efa931
Shubham Joshi <shubhamdjoshi31@gmail.com>
2024-11-22 12:33:59 +0530

    added comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,18 +9,119 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
+    const comments = await Comment.aggregate([
+        {
+            $match : {
+                video : new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $project : {
+                _id : 0,
+                content : 1
+            }
+        }
+    ])
+    
+    if (!comments?.length) {
+        throw new ApiError(400, "Unable to fetch comments")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            comments,
+            "Comments fetched successfully"
+        )
+    )
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} = req.params
+    const {content} = req.body;
+
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Video Id is invalid")
+    }
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Content is required")
+    }
+
+    const comment = await Comment.create({
+        content,
+        video : new mongoose.Types.ObjectId(videoId),
+        owner : req.user?._id
+    })
+
+    if (!comment) {
+        throw new ApiError(400, "Unable to comment")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            comment,
+            "Comment added successfully"
+        )
+    )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId} = req.params;
+    const {content} = req.body;
+
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Comment Id is invalid")
+    }
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Content is required")
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(commentId,{
+        $set : {
+            content
+        }
+    }, { new: true })
+    if (!updatedComment) {
+        throw new ApiError(400, "Could not update comment")
+    }
+
+    return res.status(200).json(new ApiResponse(200, updatedComment,"Comment updated successfully"))
 })
+
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params;
+
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Comment Id is invalid")
+    }
+
+    const deletedComment = await Comment.findByIdAndDelete(new mongoose.Types.ObjectId(commentId))
+
+    if (!deleteComment) {
+        throw new ApiError(400, "Unable to delete the comment")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            deletedComment,
+            "Comment deleted successfully"
+        )
+    )
+
 })
 
 export {
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -34,27 +34,29 @@ const publishAVideo = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
     
-    const videoLocalPath = req.files?.videoFile[0]?.path
-    const thumbnailLocalPath = req.files?.thumbnail[0]?.path
+    // const videoLocalPath = req.files?.videoFile[0]?.path
+    // const thumbnailLocalPath = req.files?.thumbnail[0]?.path
     
 
-    if (!videoLocalPath || !thumbnailLocalPath) {
-        throw new ApiError(400, "Video and thumbnail are required")
-    }
+    // if (!videoLocalPath || !thumbnailLocalPath) {
+    //     throw new ApiError(400, "Video and thumbnail are required")
+    // }
 
-    const video = await uploadOnCloudinary(videoLocalPath);
-    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+    // const video = await uploadOnCloudinary(videoLocalPath);
+    // const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
 
-    if (!video || !thumbnail) {
-        throw new ApiError(400, "Video and thumbnail are required")
-    }
+    // if (!video || !thumbnail) {
+    //     throw new ApiError(400, "Video and thumbnail are required")
+    // }
 
+    const video = "https://res.cloudinary.com/djp8zilvt/video/upload/v1732013164/samples/dance-2.mp4"
     const createdVideo = await Video.create({
         title,
         description,
-        videoFile: video.url,
-        thumbnail: thumbnail.url,
-        duration: video.duration,
+        videoFile: "https://res.cloudinary.com/djp8zilvt/video/upload/v1732013164/samples/dance-2.mp4",
+        thumbnail: "https://res.cloudinary.com/djp8zilvt/image/upload/v1732013163/samples/landscapes/landscape-panorama.jpg",
+        // duration: videoFile.duration,
+        duration : 10,
         owner: req.user?._id
     })
 
